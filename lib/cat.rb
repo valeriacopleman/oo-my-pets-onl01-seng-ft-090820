cc8f764f37b16d660dc7cc455008137d9e649b9b
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 23:53:29 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,15 +3,21 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name 
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end
   
   def mood
     @mood
   end
   
+  def self.all 
+    @@all
+  end
   
 end
