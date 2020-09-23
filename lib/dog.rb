eccb1b7435b12babdcdf815bc32bdf01bce5e023
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 23:56:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
-  # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name 
+  
+  @@all = []
+  def initialize(name, owner, mood)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self 
+  end
+  
+  def mood
+    @mood
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
 end
