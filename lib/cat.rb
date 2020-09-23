f9414bf12f76f31c50bdc502218daa077bec9112
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 23:51:48 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,10 +3,10 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name 
   
-  def initialize(name, owner, mood)
+  def initialize(name, owner, mood = nervous)
     @name = name
     @owner = owner
-    @mood = "nervous"
+    @mood 
   end
   
 end
