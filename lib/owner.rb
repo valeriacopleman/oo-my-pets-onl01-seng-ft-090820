00978e4e72d9c4c20757a2898d678194086f7d81
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-23 23:12:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,15 @@
 class Owner
   
-  attr_reader :name, :species
+  attr_reader :name
+  #, :species
   
   def initialize(name)
     @name = name
     #@species = species
   end
   
-  def species
-    @species
-  end
+  #def species
+    #@species
+  #end
   
 end
